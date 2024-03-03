f007c1d4391f0feb64d726ed556cf219d41d5c66
adam-wypych <adam.wypych.1986@gmail.com>
2024-03-03 18:49:19 +0100

    #6 adding lockable collection for 3rd party code usage to prohibit any modification for static data based planner initially after callculation of size per each group was performed

---
+++ src/main/java/org/jthreadutils/distribution/collection/immutable/LockableCollection.java
@@ -0,0 +1,63 @@
+package org.jthreadutils.distribution.collection.immutable;
+
+import java.util.Collection;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Interface for implement collections with possibility to switch on/off
+ * possibility for modification in any time user wants. This interface allows to
+ * block any modification when it shouldn't be possible, mostly in third party
+ * code.
+ * 
+ * @author adam-wypych
+ * @since 1.0.0
+ * @version %I%, %G%
+ * 
+ * @param <T> type of collection elements inside
+ */
+public interface LockableCollection<T> extends Collection<T> {
+	/**
+	 * Mark collection as immutable. All modifications method should either
+	 * thrown exception or return <code>false</code>.
+	 * 
+	 * @author adam-wypych
+	 * @since 1.0.0
+	 * @version %I%, %G%
+	 */
+	void immutable();
+
+	/**
+	 * Mark collection as mutable, all modifications method should be possible.
+	 * 
+	 * @author adam-wypych
+	 * @since 1.0.0
+	 * @version %I%, %G%
+	 */
+	void mutable();
+	
+	/**
+	 * 
+	 * @param <C>
+	 * @param action
+	 * @param isLocked
+	 * @return
+	 * 
+	 * @author adam-wypych
+	 * @since 1.0.0
+	 * @version %I%, %G%
+	 */
+	default <C> C performOperationIfNotLocked(final Callable<C> action, final AtomicBoolean isLocked) {
+		synchronized (isLocked) {
+			if (isLocked.compareAndSet(true, true)) {
+				throw new UnsupportedOperationException("Operation can't be performed given colleciton is locked for modification.");
+			} else {
+				try {
+					return action.call();
+				} catch (Exception e) {
+					throw new RuntimeException("Callable action " + action + " failed with exception.", e);
+				}
+			}
+		}
+	}
+}
